86378ad80ee7daaf12a3a863794d2878b3ebeb92
Bernhard Liebl <poke1024@gmx.de>
2021-02-17 11:48:50 +0100

    wip

--- vectorian/embeddings.py
+++ vectorian/embeddings.py
@@ -8,6 +8,7 @@ import numpy as np
 import pyarrow as pa
 import pyarrow.parquet as pq
 import os
+import download
 
 
 def _make_table(tokens, embeddings):
@@ -27,19 +28,82 @@ class Embedding:
 		return self.name, other.name, alpha
 
 
-'''
-class Precomputed:
-	def __init__(self, name, csv_path):
-		self._base_path = Path.home() / ".vectorian" / "embeddings" / "csv"
+def _load_fasttext_txt(csv_path):
+	tokens = []
+	with open(csv_path, "r") as f:
+		n_rows, n_cols = map(int, f.readline().strip().split())
 
+		embeddings = np.empty(
+			shape=(n_rows, n_cols), dtype=np.float64)
+
+		for _ in tqdm(range(n_rows)):
+			values = f.readline().strip().split()
+			if values:
+				t = values[0]
+				if t and t.isalpha() and t.lower() == t:
+					embeddings[len(tokens), :] = values[1:]
+					tokens.append(t)
+
+	embeddings = embeddings[:len(tokens), :]
+
+	embeddings = normalize(embeddings, axis=1, norm='l2')
+	embeddings = embeddings.astype(np.float32)
+
+	return tokens, embeddings
+
+
+def _load_glove_txt(csv_path):
+	tokens = []
+
+	print(f"Loading {csv_path}")
+	with open(csv_path, "r") as f:
+		text = f.read()
+
+	lines = text.split("\n")
+	n_rows = len(lines)
+	n_cols = len(lines[0].strip().split()) - 1
+
+	embeddings = np.empty(
+		shape=(n_rows, n_cols), dtype=np.float64)
+
+	for line in tqdm(lines):
+		values = line.strip().split()
+		if values:
+			t = values[0]
+			if t and t.isalpha() and t.lower() == t:
+				embeddings[len(tokens), :] = values[1:]
+				tokens.append(t)
+
+	embeddings = embeddings[:len(tokens), :]
+
+	embeddings = normalize(embeddings, axis=1, norm='l2')
+	embeddings = embeddings.astype(np.float32)
+
+	return tokens, embeddings
+
+
+class Glove(Embedding):
+	def __init__(self, name="6B"):
+		"""
+		:param name: one of "6B", "42B.300d", "840B.300d",
+		"twitter.27B", see https://nlp.stanford.edu/projects/glove/
+		"""
+
+		self._name = name
+
+		self._base_path = Path.home() / ".vectorian" / "embeddings" / "glove"
 		self._base_path.mkdir(exist_ok=True, parents=True)
 		pq_path = self._base_path / f"{name}.parquet"
 
 		if not pq_path.exists():
-			tokens, embeddings = self._load(csv_path)
+			txt_data_path = self._base_path / name
 
-			embeddings = normalize(embeddings, axis=1, norm='l2')
-			embeddings = embeddings.astype(np.float32)
+			if not txt_data_path.exists():
+				url = f"http://nlp.stanford.edu/data/glove.{name}.zip"
+				download.download(url, txt_data_path, kind="zip", progressbar=True)
+
+			tokens, embeddings = _load_glove_txt(
+				txt_data_path / f"glove.{name}.300d.txt")
 
 			pq.write_table(
 				_make_table(tokens, embeddings),
@@ -49,34 +113,23 @@ class Precomputed:
 
 		self._table = pq.read_table(pq_path)
 
-	def _load(self, csv_path):
-		tokens = []
-		with open(csv_path, "r") as f:
-			n_rows, n_cols = map(int, f.readline().strip().split())
+	@property
+	def name(self):
+		return f"glove-{self._name}"
 
-			embeddings = np.empty(
-				shape=(n_rows, n_cols), dtype=np.float64)
-
-			for _ in tqdm(range(n_rows)):
-				values = f.readline().strip().split()
-
-				t = values[0]
-				if t.isalpha() and t.lower() == t:
-					embeddings[len(tokens), :] = values[1:]
-
-					tokens.append(t)
-
-		embeddings = embeddings[:len(tokens), :]
-
-		return tokens, embeddings
-'''
+	def to_core(self):
+		return core.FastEmbedding(self.name, self._table)
 
 
 class FastText(Embedding):
 	def __init__(self, lang):
+		"""
+		:param lang: language code of fasttext encodings, see
+		https://fasttext.cc/docs/en/crawl-vectors.html
+		"""
+
 		self._lang = lang
 		self._base_path = Path.home() / ".vectorian" / "embeddings" / "fasttext"
-
 		self._base_path.mkdir(exist_ok=True, parents=True)
 		pq_path = self._base_path / f"{lang}.parquet"
 
