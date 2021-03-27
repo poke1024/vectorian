5230aa0593f589922a769f1dcf12d7001c64fd1e
Bernhard Liebl <poke1024@gmx.de>
2021-03-27 04:07:48 +0100

    wip

--- scripts/prepare_for_build.sh
+++ scripts/prepare_for_build.sh
@@ -1,7 +1,7 @@
 yum install -y conda
 
-conda create --name vectorian
-conda activate vectorian
+#conda create --name vectorian
+#conda activate vectorian
 conda info -a
 
 conda install -y -c conda-forge xtl xtensor xsimd xtensor-blas xtensor-python
