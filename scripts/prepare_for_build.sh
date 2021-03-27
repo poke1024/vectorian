4616c70af4af87825050c729b14f2fef32cb8670
Bernhard Liebl <poke1024@gmx.de>
2021-03-27 03:53:31 +0100

    wip

--- scripts/prepare_for_build.sh
+++ scripts/prepare_for_build.sh
@@ -1,9 +1,17 @@
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-source "$HOME/miniconda/etc/profile.d/conda.sh"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
+# https://docs.conda.io/projects/conda/en/latest/user-guide/install/rpm-debian.html
+rpm --import https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc
+
+cat <<EOF > /etc/yum/repos.d/conda.repo
+[conda]
+name=Conda
+baseurl=https://repo.anaconda.com/pkgs/misc/rpmrepo/conda
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc
+EOF
+
+yum install -y conda
+
 # Useful for debugging any issues with conda
 conda info -a
 
