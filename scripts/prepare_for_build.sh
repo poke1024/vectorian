ad926130f0cd364cf4a0481b105c938da776acf6
Bernhard Liebl <poke1024@gmx.de>
2021-03-27 04:03:37 +0100

    wip

--- scripts/prepare_for_build.sh
+++ scripts/prepare_for_build.sh
@@ -2,8 +2,6 @@ yum install -y conda
 
 conda create --name vectorian
 conda activate vectorian
-
-# Useful for debugging any issues with conda
 conda info -a
 
-conda install -c conda-forge xtl xtensor xsimd xtensor-blas xtensor-python
+conda install -y -c conda-forge xtl xtensor xsimd xtensor-blas xtensor-python
