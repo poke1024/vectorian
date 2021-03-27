8add637658f3a5d00253c4be21e2ae2e36d345eb
Bernhard Liebl <poke1024@gmx.de>
2021-03-27 03:57:04 +0100

    wip

--- scripts/prepare_for_build.sh
+++ scripts/prepare_for_build.sh
@@ -1,16 +1,7 @@
-# https://docs.conda.io/projects/conda/en/latest/user-guide/install/rpm-debian.html
-rpm --import https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc
-
-cat <<EOF > /etc/yum/repos.d/conda.repo
-[conda]
-name=Conda
-baseurl=https://repo.anaconda.com/pkgs/misc/rpmrepo/conda
-enabled=1
-gpgcheck=1
-gpgkey=https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc
-EOF
-
 yum install -y conda
+
+conda create --name vectorian
+conda activate vectorian
 
 # Useful for debugging any issues with conda
 conda info -a
