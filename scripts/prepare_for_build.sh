7995b91090a8901bc1d2936fa38419407571c80a
Bernhard Liebl <poke1024@gmx.de>
2021-03-27 11:03:07 +0100

    wip

--- scripts/prepare_for_build.sh
+++ scripts/prepare_for_build.sh
@@ -1,5 +1,5 @@
 yum install -y git cmake
-pip install "pybind11[global]"
+pip install "pybind11[global]" numpy
 python scripts/install_xtensor.py xtl 0.7.2
 python scripts/install_xtensor.py xtensor 0.23.4
 python scripts/install_xtensor.py xsimd 7.4.9
