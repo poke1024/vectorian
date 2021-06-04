738d05555e68e3df9acbfa1d4339844a1898d101
Bernhard Liebl <poke1024@gmx.de>
2021-06-04 15:53:51 +0200

    make sub modules available through vectorian.X

--- vectorian/__init__.py
+++ vectorian/__init__.py
@@ -7,3 +7,13 @@ def compile(for_debugging=False, force_rebuild=False):
 	if force_rebuild:
 		os.environ['DEBUG_VECTORIAN_FORCE_REBUILD'] = '1'
 	import vectorian.core
+
+
+import vectorian.embeddings as embeddings
+import vectorian.importers as importers
+import vectorian.session as session
+import vectorian.metrics as metrics
+import vectorian.alignment as alignment
+
+# "metrics" is the older name, "similarity" the newer name
+similarity = metrics
