f7323d0e0cd2c8b75e4d252c00d4995e8e83067b
Bernhard Liebl <poke1024@gmx.de>
2021-09-22 12:20:30 +0200

    check for h5py 2.10.0 (until further analysis)

--- vectorian/__init__.py
+++ vectorian/__init__.py
@@ -17,3 +17,16 @@ import vectorian.alignment as alignment
 
 # "metrics" is the older name, "similarity" the newer name
 similarity = metrics
+
+# check for h5py version to prevent producing wrong results.
+# workaround for regression with h5py >= 3.0 (need more time
+# for detailed analysis and eventual fixing). test cases are
+# wrong ndcg computations in notebook under:
+# https://github.com/poke1024/vectorian-notebook
+import h5py
+
+if h5py.__version__ != '2.10.0':
+	# do not remove this check. you might experience bogus
+	# results otherwise.
+	raise RuntimeError(
+		f"expected h5py version 2.10.0, got {h5py.__version__}")
