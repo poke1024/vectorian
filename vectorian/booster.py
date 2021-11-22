17e3b425be67dc2818649bd1dcee70ad01080678
Bernhard Liebl <poke1024@gmx.de>
2021-11-22 10:47:23 +0100

    wip boosting API design

--- vectorian/booster.py
+++ vectorian/booster.py
@@ -1,21 +1,20 @@
 import vectorian.core as core
 import numpy as np
 import scipy.signal
+import scipy.ndimage.filters
 
 from vectorian.corpus.document import PreparedDocument
 from vectorian.session import Partition
 
 
-class Convolve:
+class Filter:
 	def __call__(self, x):
 		raise NotImplementedError()
 
 
-class GaussPulse(Convolve):
-	def __init__(self, width, fc):
-		t = np.linspace(-1, 1, width, endpoint=True)
-		_, e = scipy.signal.gausspulse(t, fc=fc, retenv=True)
-		self._pulse = e / np.sum(e)
+class ConvFilter(Filter):
+	def __init__(self, pulse):
+		self._pulse = pulse / np.sum(pulse)
 
 	def __call__(self, x):
 		if self._pulse.shape[0] <= x.shape[0]:
@@ -30,9 +29,42 @@ class GaussPulse(Convolve):
 		matplotlib.pyplot.plot(self._pulse)
 
 
+class GaussFilter(ConvFilter):
+	def __init__(self, width, fc=1):
+		t = np.linspace(-1, 1, width, endpoint=True)
+		_, e = scipy.signal.gausspulse(t, fc=fc, retenv=True)
+		super().__init__(e)
+
+
+class MaxFilter(Filter):
+	def __init__(self, width):
+		self._size = width
+
+	def __call__(self, x):
+		return scipy.ndimage.filters.maximum_filter(
+			x, size=self._size)
+
+
 class Signal:
+	_filters = {
+		'gauss': GaussFilter,
+		'max': MaxFilter
+	}
+
 	def __call__(self, doc: PreparedDocument, partition: Partition):
 		raise NotImplementedError()
+
+	def filtered(self, width, method="max"):
+		return FilteredSignal(self, Signal._filters[method](width))
+
+
+class FilteredSignal(Signal):
+	def __init__(self, base, filter_):
+		self._base = base
+		self._filter = filter_
+
+	def __call__(self, doc: PreparedDocument, partition: Partition):
+		return self._filter(self._base(doc, partition))
 
 
 class KeywordSignal(Signal):
@@ -50,6 +82,15 @@ class KeywordSignal(Signal):
 		return counts.astype(np.float32) / self._max_count
 
 
+class CustomSignal(Signal):
+	def __call__(self, doc: PreparedDocument, partition: Partition):
+		spans = list(doc.spans(partition))
+		signal = self.spans_to_signal(spans)
+		assert np.max(signal) <= 1
+		assert np.min(signal) >= 0
+		return signal
+
+
 class Booster:
 	def __init__(self, strength=0.5):
 		self._f = []
@@ -58,12 +99,9 @@ class Booster:
 		if self._strength < 0 or self._strength > 1:
 			raise ValueError(f"strength has illegal value {strength}")
 
-	def add(self, signal: Signal, convolve:Convolve = None, weight:float = 1.0):
+	def add_signal(self, signal: Signal, weight:float = 1.0):
 		def compute(doc: PreparedDocument, partition: Partition):
-			w = signal(doc, partition)
-			if convolve:
-				w = convolve(w)
-			return w.astype(np.float32)
+			return signal(doc, partition).astype(np.float32)
 
 		self._f.append(compute)
 		self._w.append(weight)
