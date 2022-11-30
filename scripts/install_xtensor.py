e03b104e28f4a3119bbb41e6b4798f40524c7538
Bernhard Liebl <poke1024@gmx.de>
2022-11-30 12:41:53 +0100

    build wheel fudging

--- scripts/install_xtensor.py
+++ scripts/install_xtensor.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import pathlib
 import pybind11
+import sysconfig
 
 lib_name = sys.argv[1]
 tag_name = sys.argv[2]
@@ -11,10 +12,15 @@ pybind11.get_cmake_dir()
 repo_url = 'https://github.com/xtensor-stack/' + lib_name
 
 home = pathlib.Path.home()
+paths = sysconfig.get_paths()
 
 subprocess.check_call(['git', 'clone', repo_url], cwd=home)
 subprocess.check_call(['git', 'checkout', f'tags/{tag_name}', '-b', tag_name], cwd=home / lib_name)
 build = home / lib_name / "build"
 build.mkdir()
-subprocess.check_call(['cmake', '..'], cwd=build)
+subprocess.check_call([
+	'cmake',
+	f'-DPYTHON_INCLUDE_DIR={paths["include"]}',
+	f'-DPYTHON_EXECUTABLE={sys.executable}',
+	'..'], cwd=build)
 subprocess.check_call(['make', 'install'], cwd=build)
